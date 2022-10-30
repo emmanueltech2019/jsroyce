c16e4a4ddb6c489825a995540e7910bdfd910f58
System Administrator <root@Devemmys-MacBook-Pro.local>
2022-10-30 16:37:25 +0100

    updated packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+  
+// }
+
+
+const nextConfig =withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+    reactStrictMode:true,
+    swcMinify:true,
+    images: {
+      unoptimized: true,
+    },
+  }],
+  // your other plugins here
+
+]);
 module.exports = nextConfig
